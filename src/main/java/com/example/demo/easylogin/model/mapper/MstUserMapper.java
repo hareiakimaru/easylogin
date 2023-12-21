095e955f1052c5e39205c810d88167203db8f3a3
hareiakimaru <g2017412@outlook.jp>
2023-12-21 21:36:03 +0900

    Lesson_easyLogin

---
+++ src/main/java/com/example/demo/easylogin/model/mapper/MstUserMapper.java
@@ -0,0 +1,13 @@
+package com.example.demo.easylogin.model.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.example.demo.easylogin.model.domain.MstUser;
+
+@Mapper
+public interface MstUserMapper {
+
+	@Select("SELECT * FROM mst_user WHERE user_name = #{userName} AND password = #{password}")
+	MstUser findByUserNameAndPassword( String userName, String password);
+}
